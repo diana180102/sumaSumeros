42c4d8e2e899cf326171097d33745cf200b992cd
Diana Mayorga <diana101210@gmail.com>
2022-04-16 18:00:17 -0500

    Cargando Proyecto de Suma de números

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int resultado = suma(3, 4, 8);
+
+        System.out.println(resultado);
+
+
+
+    }
+
+    public static int suma (int a, int b, int c){
+
+        return a + b + c;
+    }
+}
+
+
+
